fb1fe79f75e165a31f2e3a47d4f7cc362a5c8218
mobin.francis <mobin.francis@BDC7-D-5T2QXH2.dir.svc.accenture.com>
2019-01-14 16:43:56 +0530

    committed on jan 14th from office

--- SeleniumLearning/src/chrometest.java
+++ SeleniumLearning/src/chrometest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -6,7 +7,14 @@ public class chrometest {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","C:\\drivers\\chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
-		driver.get("http://zerodha.com");
+//		driver.get("http://zerodha.com");
+//		System.out.println(driver.getCurrentUrl());// Printing current url
+////		System.out.println(driver.getPageSource());
+//		driver.get("https://www.guru99.com/");
+//		driver.navigate().back();
+		driver.get("http://globaleducation.net.in/");
+		driver.findElement(By.id("name")).sendKeys("mobin");
+		
 
 	}
 
