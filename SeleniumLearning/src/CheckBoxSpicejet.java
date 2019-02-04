2f9d115cc89567140053c511d0a00712e0da63c0
mobin.francis <mobin.francis@BDC7-D-5T2QXH2.dir.svc.accenture.com>
2019-02-04 16:52:09 +0530

    at evening

--- SeleniumLearning/src/CheckBoxSpicejet.java
+++ SeleniumLearning/src/CheckBoxSpicejet.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class CheckBoxSpicejet {
 
@@ -8,13 +9,13 @@ public class CheckBoxSpicejet {
 		System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.spicejet.com/");
-		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
-		
+		//By default value is false
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		//System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
 		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
-		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		//System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		Assert.assertEquals(driver.findElements(By.cssSelector("*[ type='checkbox']")).size(), 6);
 		System.out.println(driver.findElements(By.cssSelector("*[ type='checkbox']")).size());
-		
-
 	}
 
 }
